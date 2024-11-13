e3999ba1ef148f96896c0e1a285c0aaf94403b93
Yaso1964 <byaso@yahoo.com>
2024-11-13 13:51:39 +0000

    Update BrowserTest.java

--- src/test/java/Academy/BrowserTest.java
+++ src/test/java/Academy/BrowserTest.java
@@ -23,6 +23,7 @@ public class BrowserTest {
 		String text =driver.findElement(By.cssSelector("h1")).getText();
 		System.out.println(text);
 		Assert.assertTrue(text.equalsIgnoreCase("RahulShettyAcademy.com Learning"));
+		Thread.sleep(60000);
 		driver.close();
 	
 		
