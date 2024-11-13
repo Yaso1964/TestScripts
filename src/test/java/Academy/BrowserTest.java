9204d130f343ff8f26b88a4d8e34800972f21647
Yaso1964 <byaso@yahoo.com>
2024-11-13 11:22:37 +0000

    Update BrowserTest.java

--- src/test/java/Academy/BrowserTest.java
+++ src/test/java/Academy/BrowserTest.java
@@ -17,7 +17,7 @@ public class BrowserTest {
 	public void getData()
 	{
 		System.out.println("Hello Guys");
-		System.setProperty("webdriver.chrome.driver", "/Users/fionagwaze/Downloads/chromedriver-mac-x64/chromedriver");
+		System.setProperty("webdriver.chrome.driver", "/Users/fionagwaze/Downloads/chromedriver-mac-x64/chromedriver.exec");
 		WebDriver driver =new ChromeDriver();
 		driver.get("https://rahylshettyacademy.azurewebsites.net/webapp/");
 		String text =driver.findElement(By.cssSelector("h1")).getText();
